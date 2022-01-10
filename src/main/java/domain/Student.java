ed52b3348c3798d694912fef9f0a6bb39aadea15
agravitybrain <tsyaroslav.988@gmail.com>
2022-01-10 21:03:18 +0200

    everything except pom.xml

--- src/main/java/domain/Student.java
+++ src/main/java/domain/Student.java
@@ -2,18 +2,36 @@ package domain;
 
 import json.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class Student extends BasicStudent {
+    private List<Json> exams;
 
     public Student(String name, String surname, Integer year, Tuple<String, Integer>... exams) {
-        // ToDo
+        super(name, surname, year);
+        this.exams = new ArrayList<>();
+        for (Tuple<String, Integer> exam: exams) {
+            String subject = exam.key;
+            Integer mark = exam.value;
+            this.exams.add(new JsonObject(
+                    new JsonPair("course", new JsonString(subject)),
+                    new JsonPair("mark", new JsonNumber(mark)),
+                    new JsonPair("passed", new JsonBoolean(mark >= 3))
+            ));
+        }
     }
 
     @Override
     public JsonObject toJsonObject() {
-        // ToDo
-        return null;
+        return new JsonObject(
+                new JsonPair("name", new JsonString(name)),
+                new JsonPair("surname", new JsonString(surname)),
+                new JsonPair("year", new JsonNumber(year)),
+                new JsonPair("exams", new JsonArray(exams.toArray(new Json[0])))
+        );
     }
 }
